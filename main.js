16a29d440779e13d6c4364959d133a76c9894995
1ory-96yy <136627889+1ory-96yy@users.noreply.github.com>
2025-06-22 19:19:23 +0200

    2

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
- document.getElementById('usernameInput').addEventListener('input', function(e) {
-            this.value = this.value.replace(/[0-9]/g, '');
+const openBtn = document.getElementById('openModalBtn');
+        const closeBtn = document.getElementById('closeModalBtn');
+        const modal = document.getElementById('modal');
+        const overlay = document.getElementById('overlay');
+        
+        openBtn.addEventListener('click', function() {
+            modal.style.display = 'block';
+            overlay.style.display = 'block';
+        });
+        
+        closeBtn.addEventListener('click', function() {
+            modal.style.display = 'none';
+            overlay.style.display = 'none';
         });
