56842de5d555e500a9addd3eb61f74161abd8c6d
1ory-96yy <136627889+1ory-96yy@users.noreply.github.com>
2025-06-22 19:31:30 +0200

    6

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
- document.getElementById('usernameInput').addEventListener('input', function(e) {
-            this.value = this.value.replace(/[0-9]/g, '');
+document.addEventListener('DOMContentLoaded', function() {
+            const buttons = document.querySelectorAll('.button-container');
+            buttons.forEach(container => {
+                const button = container.querySelector('button');
+                const tooltip = container.querySelector('.tooltip');
+                const buttonRect = button.getBoundingClientRect();
+                const tooltipRect = tooltip.getBoundingClientRect();
+                if (buttonRect.top - tooltipRect.height < 0) {
+                    tooltip.classList.remove('top');
+                    tooltip.classList.add('bottom');
+                }
+            });
         });
